04b5244ddbf32bb7137e61c92e8ba2c00600356a
DesmoVale <valerioleone2003.vl@gmail.com>
2023-03-24 13:59:06 +0100

    Aggiornamenti login page

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 function qr() {
+
+
   var name = document.getElementById("name").value;
   var surname = document.getElementById("surname").value;
-  var email = document.getElementById("email").value;
-  if(name == "" || surname == "" || email == ""){
-    alert("Inserire correttamente dati")
+  var date = document.getElementById("birth").value;
+  var emailInput = document.getElementById("email");
+  var email = emailInput.value;
+  var regex = /^\S+@\S+\.\S+$/;
+  if (!regex.test(email) || name == "" || surname == "") {
+      alert("Inserire correttamente dati")
   } else {
-    var data = name + " " + surname + ", " + email;
-    var qrcode = new QRCode(document.getElementById("qrcode"), {
-    text: data,
-    width: 256,
-    height: 256,
-    colorDark : "#000000",
-    colorLight : "#ffffff",
-    correctLevel : QRCode.CorrectLevel.H
+    // La mail è valida, procedere con la registrazione
+      var data = name + ", " + surname + ", " + email + ", " + date;
+      var qrcode = new QRCode(document.getElementById("qrcode"), {
+      text: data,
+      width: 256,
+      height: 256,
+      colorDark : "#000000",
+      colorLight : "#ffffff",
+      correctLevel : QRCode.CorrectLevel.H
     });
   
   var canvas = document.getElementById("qrcode").querySelector("canvas");
   if (canvas) {
     downloadBtn.disabled = false;
   }
-}
+  document.getElementById("container").style.display = "block";
+  }
+  
+
   
 downloadBtn.addEventListener("click", function() {
   var canvas = document.getElementById("qrcode").querySelector("canvas");
